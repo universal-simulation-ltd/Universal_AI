33ffe44abee681a897134e1716aa22ba2bc6d5fc
James Markey <Inbox@jamesmarkey.co.uk>
2026-07-05 09:31:46 +0100

    chore(ios): change bundle identifier to com.universal-ai.app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ltd.universalsimulation.ai',
+  appId: 'com.universal-ai.app',
   appName: 'Universal AI',
   webDir: 'dist'
 };
